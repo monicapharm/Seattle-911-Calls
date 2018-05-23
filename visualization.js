66521672d4c6d38cf4ca1d5c563fa1bffbdd25de
Joel Ross <joelross@uw.edu>
2018-05-23 09:13:11 -0700

    Add starter files

---
+++ visualization.js
@@ -0,0 +1,11 @@
+'use strict';
+
+//the SVG element to add visual content to
+var svg = d3.select('#visContainer')
+		.append('svg')
+		.attr('height', 480) //can adjust size as desired
+		.attr('width', 600)
+    .style('border','1px solid gray'); //comment out to remove border
+
+/* Your script goes here */
+
